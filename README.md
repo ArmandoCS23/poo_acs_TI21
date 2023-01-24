c910bb53ff78ee5b4bfcc7bb08f45c2a35f4a530
ARMANDO CASTELAN <122809483+ArmandoCS23@users.noreply.github.com>
2023-01-24 20:46:28 +0000

    Se creó el archivo llamado programa3.py

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 |1.|programa0.py|Hola mundo en python|
 |2.|programa1.py|Comentarios multilinea, comentarios de una linea y concatenacion de cadenas|
 |3.|programa2.py|Concatenacion usando la funcion format|
+|4.|programa3.py|Print y Operaciones Aritmétcas|
---
+++ semana2/programa3.py
@@ -0,0 +1,25 @@
+"""
+       Programa3
+       Nombre: Armando CS
+       Fecha: 24/01/2023
+       Descripcion: Print y Operaciones Aritmétcas
+"""
+numero1 = 10
+numero2 = 5
+print(numero1 + numero2)
+print("{} + {}".format(numero1 , numero2))
+print("{} + {} = {}".format(numero1 , numero2 , (numero1 + numero2)))
+
+numero3 = 5
+numero4 = 10
+print(numero3 * numero4)
+print("{} * {}".format(numero3 , numero4))
+print("{} = {} * {}".format((numero3 * numero4) , numero3 , numero4))
+
+variable1 = "El resultado de dividir"
+variable2 = "es"
+numero5 = 10
+numero6 = 5
+print(numero5 / numero6)
+print("{} / {}".format(numero5 , numero6))
+print("{} {} / {} {} {}".format(variable1 , numero5 , numero6 , variable2 , (numero5 / numero6)))
