b237636173bdb9616a73e4ee17b8fb47889d724b
ARMANDO CASTELAN <122809483+ArmandoCS23@users.noreply.github.com>
2023-01-24 19:40:41 +0000

    Se creó el archivo llamado programa1.py

---
+++ semana2/programa1.py
@@ -0,0 +1,9 @@
+"""
+       Programa1
+       Nombre: Armando CS
+       Fecha: 23/01/2023
+       Descripcion: Comentarios multilinea, comentarios de una linea y concatenacion de cadenas
+"""
+variable1 = "Hola"  # Variable que almacena una cadena de caracteres. STR
+variable2 = '619'  # Variable que almacena una cadena de caracteres. INT
+print(variable1 + variable2)  # Imprime la concatenacion de 2 variables de tipo str 
