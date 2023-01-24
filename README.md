285943f30f32bb288a5f05c2b12e35db8b5bd2fe
ARMANDO CASTELAN <122809483+ArmandoCS23@users.noreply.github.com>
2023-01-24 19:58:38 +0000

    Se creó un nuevo archivo llamado programa2.py

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 |--|--|--|
 |1.|programa0.py|Hola mundo en python|
 |2.|programa1.py|Comentarios multilinea, comentarios de una linea y concatenacion de cadenas|
+|3.|programa2.py|Concatenacion usando la funcion format|
---
+++ semana2/programa2.py
@@ -0,0 +1,9 @@
+"""
+    Programa2
+       Nombre: Armando CS
+       Fecha: 24/01/2023
+       Descripcion: Concatenacion usando la funcion format
+""" 
+variable1 = "Hola"  
+variable2 = 619
+print("{} {}".format(variable1 , variable2))
