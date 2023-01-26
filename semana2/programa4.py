fc833f3e94a0a628d3976883cfcb0b42167fbdea
ARMANDO CASTELAN <122809483+ArmandoCS23@users.noreply.github.com>
2023-01-26 19:51:08 +0000

    Se creó el archivo llamado programa4.py

---
+++ semana2/programa4.py
@@ -0,0 +1,11 @@
+"""
+       Programa4
+       Nombre: Armando CS
+       Fecha: 26/01/2023
+       Descripcion: 
+"""
+numero1 = 10
+numero2 = 5
+print("{} + {} = {}".format(numero1, numero2 , numero1 + numero2))
+
+print("{n1} + {n2} = {suma}".format(n1 = numero1, n2 = numero2, suma = numero1 + numero2))
