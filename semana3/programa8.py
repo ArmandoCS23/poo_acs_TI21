1f6a67748c870742ba133d3e44a7f9171fdb1a57
ARMANDO CASTELAN <122809483+ArmandoCS23@users.noreply.github.com>
2023-02-02 20:54:47 +0000

    Se creó el programa9 para comparar dos números y se actualizó el programa8.py

--- semana3/programa8.py
+++ semana3/programa8.py
@@ -4,24 +4,18 @@
        Fecha: 02/02/2023
        Descripcion: Condición IF
 """
-{
   if condición:
       código
-}
 
-{
   if condicion:
      codigo1
   else:
      codigo2
-}
 
 
-{
   if condición:
       código1
   elif condicion2:
       código2
   else:
       código3
-}
---
+++ semana3/programa9.py
@@ -0,0 +1,20 @@
+"""
+       Programa9
+       Nombre: Armando CS
+       Fecha: 02/02/2023
+       Descripcion: Realizar un programa en Python que compare 2 números enteros e imprima el 
+       mayor de ellos, y si son iguales imprima None
+"""
+n1=int(input("num1: ")) #variable tipo entero
+n2=int(input("num2: ")) #variable tipó en tero
+
+if n1 < n2:
+    print(n2) #variable tipo int
+if n1 > n2:
+    print(n1) #variable tipo int
+if n1 == n2:
+    print(None) #variable tipo str
+
+    
+  
+    
